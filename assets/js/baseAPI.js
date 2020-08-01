37035f46b15e32c9c6de68f9a7c0853b0c04e244
unknown <690580101@qq.com>
2020-08-01 14:37:26 +0800

    提交index分支

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,4 +3,20 @@
 $.ajaxPrefilter(function(options){
     // 在发起真正的ajax请求之前，统一拼接请求的根路径
     options.url = 'http://ajax.frontend.itheima.net' + options.url
+
+    // 判断，请求路径是否包含/my/
+    if(options.url.indexOf('/my/') !== -1){
+        options.headers = {
+            Authorization:localStorage.getItem('token') || ''
+        }
+    }
+
+    // 所有的请求完成后都要进行身份验证
+    options.complete = function(res){
+        var data = res.responseJSON
+        if(data.status == 1 && data.message == '身份认证失败！') {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,49 @@
+$(function(){
+    // 调用函数获取用户的基本信息
+    getUserInfo()
+
+    // 退出功能
+    var layer = layui.layer
+    $("#btnLogout").on('click',function(){
+        layer.confirm('确认退出?', {icon: 3, title:'提示'}, function(index){
+            //do something
+            layer.close(index);
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+        });
+    })
+})
+
+// 封装获取用户的基本信息函数
+function getUserInfo(){
+    $.ajax({
+        method:'get',
+        url:'/my/userinfo',
+        // headers 就是请求头配置对象
+        // headers:{
+        //     Authorization:localStorage.getItem('token') || ''
+        // },
+        success:function(res){
+            if( res.status != 0 )return layui.layer.msg(res.message)
+            // 调用 renderAvatar 渲染头像
+            renderAvatar(res.data)
+        },
+    })
+}
+
+// 封装渲染用户头像的函数
+function renderAvatar (user){
+    // 获取用户名称
+    var uname = user.nickname || user.username
+    $("#welcome").html('欢迎&nbsp;&nbsp;' + uname)
+    // 获取用户头像
+    // 判断是否有设置头像
+    if(user.user_pic != null){
+        $(".layui-nav-img").show().attr("src",user.user_src)
+        $(".text-avatar").hide()
+    }else {
+        $(".layui-nav-img").hide()
+        $(".text-avatar").show().html(uname[0].toUpperCase())
+    }
+}
+
